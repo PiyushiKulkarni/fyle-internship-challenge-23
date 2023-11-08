7a8bc24eeb8e6eb0c398369c44ef2b53abc39d27
Piyushi Kulkarni <112570663+PiyushiKulkarni@users.noreply.github.com>
2023-11-08 22:44:46 +0530

    Update api.service.ts

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,19 +1,46 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { tap, throwError } from 'rxjs';
+import { catchError, tap, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
+
+
 export class ApiService {
 
-  constructor(
-    private httpClient: HttpClient
-  ) { }
+  constructor(private httpClient: HttpClient) { }
+  
+  username: string='';
+
+  handleError(error: HttpErrorResponse) {
+    let errorMessage = "";
+    if (error.status === 0) {
+    errorMessage = 'An error occurred: Server unreachable';
+    return throwError(() => new Error(errorMessage));
+    } else {
+      errorMessage =   ` ${error.status} `, error.error;
+    }
+    errorMessage += 'Please try again later.';
+    return throwError(() => new Error(errorMessage));
+  }
+
 
   getUser(githubUsername: string) {
-    return this.httpClient.get(`https://api.github.com/users/${githubUsername}`);
+    return this.httpClient.get(`https://api.github.com/users/${githubUsername}`).pipe(
+      catchError(this.handleError)
+    );
   }
 
   // implement getRepos method by referring to the documentation. Add proper types for the return type and params 
+  getReposListApi(username: string , page: number, per_page:number) {
+    return this.httpClient.get(`https://api.github.com/users/${username}/repos?page=${page}&per_page=${per_page}` , {observe:'response'}).pipe(
+      catchError(this.handleError)
+    );
+}
+
+  setUsername(username:string){
+    this.username = username;
+  }
+
 }
