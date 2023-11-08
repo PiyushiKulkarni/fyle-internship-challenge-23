d8314894a5759d217c7f6673bbc1780c47fd8e55
Piyushi Kulkarni <112570663+PiyushiKulkarni@users.noreply.github.com>
2023-11-08 22:55:37 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from  '@angular/common/http';
-
+import { HttpClientModule } from '@angular/common/http';
+import {RepolistComponent} from './components/repo-list/repo-list.component';
+import { HomeComponent } from './components/home/home.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RepolistComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
+    FormsModule,
     HttpClientModule
   ],
   providers: [],
